dfcb1f5333381208b6e0aa5c4fb24e88aadc5f93
Bareerah-zulquarnain <bareerah.zulquarnain@gmail.com>
2024-12-08 20:00:50 +0500

    Add files via upload

---
+++ ImplementBitwise/main.py
@@ -0,0 +1,27 @@
+num1=10
+num2=4
+print("num1 & num2="num1 &num)
+print("\nnum1|num2=",num1|num2)
+print("\n-num1=",num1)
+print("\nnum1 ^ num2 =", num1 ^ num2)
+ 
+ 
+ 
+num1 = 10
+num2 = 4
+ 
+# Using Right Shift operator on num1
+print("\nnum1 >> 1 =", num1 >> 1)
+ 
+# Using Right Shift operator on num2
+print("\nnum2 >> 1 =", num2 >> 1)
+ 
+num1 =
+num1 = 10
+num2 = 4
+ 
+# Using Left Shift  operator on num1
+print("\nnum1 << 1 =", num1 << 1)
+ 
+# Using Left Shift operator on num2
+print("\nnum2 << 1 =", num2 << 1)
